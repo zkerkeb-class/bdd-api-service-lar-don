47a1142609402522bd075c24aaae81c731ff4624
RyanPeyrot <ryan.peyrot@gmail.com>
2024-04-23 22:13:31 +0200

    Ajout en BDD de la gestion de l'historique des chats

---
+++ src/controllers/chat.controller.js
@@ -0,0 +1,70 @@
+const Chat = require('../models/chat.model');
+
+exports.create = async (req, res) => {
+    const chat = new Chat([{role: 'system', content: req.body.content}]);
+    try {
+        const savedChat = await chat.save();
+        return res.status(201).json(savedChat);
+
+    } catch (error) {
+        return res
+            .status(500)
+            .json(error);
+    }
+}
+
+exports.delete = async (req, res) => {
+    const chatId = req.params.id;
+    try{
+        const chat = await Chat.findByIdAndDelete(chatId);
+        if(!chat){
+            return res.status(404).json({ message: 'Chat non trouvé' });
+        }
+        return res
+            .status(200)
+            .json({ message: 'Chat supprimé avec succès' });
+    } catch (error) {
+        console.error(error);
+        return res
+            .status(500)
+            .json({ message: "Erreur lors de la suppression du chat" });
+    }
+
+}
+
+exports.getOne = async (req, res) => {
+    const chatId = req.params.id;
+
+    try {
+        const chat = await Chat.findById(chatId);
+        if (!chat) {
+            return res.status(404).json({ message: 'Chat non trouvé' });
+        }
+
+        res.status(200).json(chat);
+    } catch (error) {
+        console.error(error);
+        return res
+            .status(500)
+            .json({ message: "Erreur lors de la récupération du chat" });
+    }
+}
+
+exports.updateOne = async (req, res) => {
+    const chatId = req.params.id;
+    const history = req.body.history;
+
+    try {
+        const chat = await Chat.findByIdAndUpdate(chatId,{$push:{ history:{ $each:history}}}, {new: true});
+        if (!chat) {
+            return res.status(404).json({ message: 'Chat non trouvé' });
+        }
+
+        res.status(200).json(chat);
+    } catch (error) {
+        console.error(error);
+        return res
+            .status(500)
+            .json({ message: "Erreur lors de la récupération du chat" });
+    }
+}
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 const userRouter = require('./user.route')
+const chatRouter = require('./chat.route')
+
 
 router.use('/users',userRouter);
+router.use('/chat',chatRouter);
+
 
 module.exports = router;
