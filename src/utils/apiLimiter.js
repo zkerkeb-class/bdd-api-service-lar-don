d2e941b27094e3f68c52bc638c846f472227e2d7
Aubin Olivrie <aubin.olivrie@smice.com>
2024-04-25 00:23:31 +0200

    [FIX] Higher rate limit

--- src/utils/apiLimiter.js
+++ src/utils/apiLimiter.js
@@ -2,7 +2,7 @@ const rateLimit = require('express-rate-limit');
 
 const apiLimiter = rateLimit({
   windowMs: 1 * 60 * 1000, // 1 minute
-  max: 10, // limit each IP to 10 requests per windowMs
+  max: 30, // limit each IP to 10 requests per windowMs
   message: {
     status: 429,
     message: 'Trop de requêtes, veuillez réessayer dans une minute',
