794fa8116dd9d2c1de5c80e7a0b4204bdfe02076
RyanPeyrot <ryan.peyrot@gmail.com>
2024-04-24 23:34:23 +0200

    push all

--- src/controllers/chat.controller.js
+++ src/controllers/chat.controller.js
@@ -1,7 +1,7 @@
 const Chat = require('../models/chat.model');
 
 exports.create = async (req, res) => {
-    const chat = new Chat([{role: 'system', content: req.body.content}]);
+    const chat = new Chat({history :[{role: 'system', content: req.body.content}]});
     try {
         const savedChat = await chat.save();
         return res.status(201).json(savedChat);
@@ -37,6 +37,24 @@ exports.getOne = async (req, res) => {
 
     try {
         const chat = await Chat.findById(chatId);
+        if (!chat) {
+            return res.status(404).json({ message: 'Chat non trouvé' });
+        }
+
+        res.status(200).json(chat);
+    } catch (error) {
+        console.error(error);
+        return res
+            .status(500)
+            .json({ message: "Erreur lors de la récupération du chat" });
+    }
+}
+
+exports.getAll = async (req, res) => {
+
+    try {
+        const chat = await Chat.find()
+        console.log(chat);
         if (!chat) {
             return res.status(404).json({ message: 'Chat non trouvé' });
         }
--- src/routes/chat.route.js
+++ src/routes/chat.route.js
@@ -5,6 +5,7 @@ const controller = require("../controllers/chat.controller")
 
 //router.get(<path>,<controller>.<method>)
 router.post("/",controller.create);
+router.get("/all",controller.getAll);
 router.get("/:id",controller.getOne);
 router.delete("/:id",controller.delete);
 router.put('/:id',controller.updateOne)
